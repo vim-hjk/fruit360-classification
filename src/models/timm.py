ab842ab39a651ffd3b0fe48fd248a51d9b0d400b
HyoJin Kim <77153072+vim-hjk@users.noreply.github.com>
2021-06-14 22:25:49 +0900

    Update timm.py

--- src/models/timm.py
+++ src/models/timm.py
@@ -6,7 +6,7 @@ import torch.nn as nn
 # pprint(timm.list_models(pretrained=True))
 
 class PretrainedModel(nn.Module):
-    def __init__(self, model_arc='efficientnet_b0', num_classes=131):
+    def __init__(self, model_arc='resnet18d', num_classes=131):
         super().__init__()
         self.net = timm.create_model(model_arc, pretrained=True, num_classes=num_classes)
     
